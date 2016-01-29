794b5a1297666ae43715b1017d7d94a0ffa6ad32
Michael Duran <mad2116@gmail.com>
2016-01-29 12:56:45 -0800

    adds all mods

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 console.log('main.js loaded!');
+
+var paused = false;
+var win = false;
+var grid = [];
+var cell = {
+                state: "avocado", //empty, avo, guac
+                position: "4",
+};
+
+if (state = avo) {
+  win = false;
+}
+
+/*
+Render:
+when to render?
+  -game is won*
+  -game starts*
+  -when click on avo*
+  -when avo appears // triggered by a tick render
+  -when pause*
+  -when restart* // * is a user driven event
+
+when render, what data?
+  -paused = false
+  -win = false
+  -start = false
